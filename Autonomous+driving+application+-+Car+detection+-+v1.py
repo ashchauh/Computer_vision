feb50a1e2baa283acef40038901cb11d869eb0fa
Ashish Chauhan <ashish222322@gmail.com>
2018-05-09 14:50:24 -0400

    Update Autonomous+driving+application+-+Car+detection+-+v1.py

--- Autonomous+driving+application+-+Car+detection+-+v1.py
+++ Autonomous+driving+application+-+Car+detection+-+v1.py
@@ -1,12 +1,7 @@
 
 # # Autonomous driving - Car detection
-# 
-# Welcome to your week 3 programming assignment. You will learn about object detection using the very powerful YOLO model. Many of the ideas in this notebook are described in the two YOLO papers: Redmon et al., 2016 (https://arxiv.org/abs/1506.02640) and Redmon and Farhadi, 2016 (https://arxiv.org/abs/1612.08242). 
-# 
-# **You will learn to**:
 # - Use object detection on a car detection dataset
 # - Deal with bounding boxes
-# 
 # Run the following cell to load the packages and dependencies that are going to be useful for your journey!
 
 
